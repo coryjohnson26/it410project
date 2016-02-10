e78d8b5eb39ef1cc1b5c67454c4aa2299b7f0792
Cory Johnson <coryaj@byu.edu>
2016-02-09 20:04:24 -0700

    attempt to call connection

--- user-management-system/config/mongodb.js
+++ user-management-system/config/mongodb.js
@@ -5,12 +5,15 @@ const assert = require('assert');
 var mongodb;
 var url = 'mongodb://localhost:27017/userManagement';
 
-MongoClient.connect(url, function (err, db) {
-	assert(err, null);
-	mongodb = db;
-});
+var connect = function(){
+	MongoClient.connect(url, function (err, db) {
+		assert(err, null);
+		mongodb = db;
+	});
+}
 
 exports.addUser = function(user, pass){
+	connect();
 	return new Promise(function(resolve, reject){
 		mongodb.collection('users').insertOne({user: user, pass: pass}, function(err, record){
 			if(err) return reject(err);
