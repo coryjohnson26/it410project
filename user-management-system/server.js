3938824c7f1720a0c25724ee6574b9059bd4bf65
Cory Johnson <coryaj@byu.edu>
2016-02-16 21:16:42 -0700

    User management web services

--- user-management-system/server.js
+++ user-management-system/server.js
@@ -7,9 +7,26 @@ var port = process.env.PORT || 3000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/', function(req, res){
-	res.send('get')
+app.get('/services/user', function(req, res){
+	res.send('get user')
 })
+
+app.post('/services/user', function(req, res){
+	res.send('post user')
+})
+
+app.put('/services/user', function(req, res){
+	res.send('put user')
+})
+
+app.put('/services/login', function(req, res){
+	res.send('put login')
+})
+
+app.put('/services/logout', function(req, res){
+	res.send('put logout')
+})
+
 
 app.listen(port, function(){
 	console.log('Example app listening on port ' + port)
---
+++ user-management-web-services/src/users.js
@@ -0,0 +1,22 @@
+const db = require('../config/mongodb');
+const Promise = require('bluebird');
+
+exports.createUser = function(user, pass){
+	return db.addUser(user,pass);
+};
+
+exports.updateUser = function(user, pass){
+	return db.updateUser(user,pass);
+};
+
+exports.authUser = function(user, pass){
+	return db.verifyUser(user,pass);
+};
+
+exports.findUser = function(user){
+	return db.findUser(user);
+};
+
+exports.clear = function(username){
+	return db.remove(username);
+}
